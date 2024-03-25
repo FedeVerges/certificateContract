746f1e124c099beeeb1df53c206a2d96ea513da2
Fede <fedeverges@gmail.com>
2024-03-24 21:21:11 -0300

    ajustes test

--- test/CertificateContract.js
+++ test/CertificateContract.js
@@ -21,8 +21,7 @@ contract("Certificate", () => {
 
     it("Prueba de idempotencia: No se pueden crear dos titulos iguales (prueba numero de oblea.)", async () => {
 
-        // Datos del certificado que deseas crear
-
+        // Datos titulo.
         const certificate1 = {
             student: {
                 docNumber: "41221778",
@@ -47,30 +46,6 @@ contract("Certificate", () => {
             waferNumber: "123123213",
         };
 
-        const certificate = {
-            id: 0,
-            student: {
-                id: 123453000000,
-                name: "Nombre",
-                lastname: "Apellido",
-                docType: 'DNI',
-                docNumber: "3000000",
-                sex: "M",
-                registrationNumber: 1235,
-            },
-            universityDegree: {
-                universityName: "Universidad",
-                academicUnit: "Facultad",
-                degreeProgramName: "Ingeniería en Informatica",
-                degreeProgramCurriculum: "12-34",
-                degreeType: "Postgrado",
-            },
-            waferNumber: "1234ABCDEF",
-            createdAt: 0,
-            updatedAt: 0,
-            active: true
-        };
-
 
         // Llama a la función para crear el certificado.
         await this.certificateContract.createCertificate(certificate1);
@@ -78,22 +53,23 @@ contract("Certificate", () => {
         // Verifica que el evento "CertificateCreated" haya sido emitido.
         const events = await this.certificateContract.getPastEvents("CertificateCreated");
         assert.equal(events.length, 1, "El evento CertificateCreated no se emitió correctamente.");
-        console.log(events)
+        console.log(events);
 
         // Verifica que el certificado fue creado correctamente.
         const certificates = await this.certificateContract.getCertificatesByStudentId(certificate1.student.id);
 
-        // console.log(JSON.stringify(certificate));
-
         assert.lengthOf(certificates, 1, "Se ha creado el certificado correctamente.");
         try {
-            // Vuelvo a crear el mismo certificado.
-            await this.certificateContract.createCertificate(certificate);
+            // Vuelvo a cargar el mismo titulo.
+            await this.certificateContract.createCertificate(certificate1);
+
             // Si continua normalmente, arrojo error.
             throw new Error('No se pueden crear dos certificados con el mismo numero de oblea.');
 
         } catch (error) {
-            console.error("Control de idempotencia exitoso. No se pueden cargar dos certificados iguales. Transaccion revertida.", error.message);
+            console.info(
+                "Control de idempotencia exitoso. No se pueden cargar dos certificados iguales. Transaccion revertida.",
+                error.message);
         }
 
     });
